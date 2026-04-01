885df3e364ecb45b74978f68c7b7b08573e72c5d
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-03-31 21:22:40 -0500

    M - feat: add repository

--- src/main/java/com/gestion/micromarket/repository/EmployeesRepository.java
+++ src/main/java/com/gestion/micromarket/repository/EmployeesRepository.java
@@ -1,5 +1,14 @@
 package com.gestion.micromarket.repository;
 
-public class EmployeesRepository {
-    
-}
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gestion.micromarket.entity.Employees;
+
+@Repository
+public interface EmployeesRepository extends JpaRepository<Employees, Long>{
+    Optional<Employees> findByDocumentNumber(String documentNumber);
+
+} 
