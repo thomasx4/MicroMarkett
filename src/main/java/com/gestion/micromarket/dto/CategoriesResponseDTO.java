b72d89414e55dd59e5e95bb3b749c29b2f5951d7
krisgallego <krisgallego7@gmail.com>
2026-03-31 21:13:31 -0500

    KM - feat: CategoriesRequestDTO and CategoriesResponseDTO DTO

--- src/main/java/com/gestion/micromarket/dto/CategoriesResponseDTO.java
+++ src/main/java/com/gestion/micromarket/dto/CategoriesResponseDTO.java
@@ -1,5 +1,42 @@
 package com.gestion.micromarket.dto;
 
+import java.time.LocalDateTime;
+
 public class CategoriesResponseDTO {
-    
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt = createdAt;
+    }
 }
