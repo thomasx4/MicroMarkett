eab3166f6f9eb55928624c69d79c863225d54f47
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-04-06 16:47:02 -0500

    M - feat: Add salesdetails repository file

--- src/main/java/com/gestion/micromarket/repository/SaleDetailRepository.java
+++ src/main/java/com/gestion/micromarket/repository/SaleDetailRepository.java
@@ -1,5 +1,13 @@
 package com.gestion.micromarket.repository;
 
-public class SaleDetailRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gestion.micromarket.entity.SaleDetail;
+
+@Repository
+public interface SaleDetailRepository extends JpaRepository<SaleDetail, Long> {
+    List<SaleDetail> findBySaleId(Long saleId);
 }
