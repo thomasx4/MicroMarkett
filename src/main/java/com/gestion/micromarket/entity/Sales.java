4e8154f2ae1ced927c20b8a2ab146b8cc60583f7
juanmorenaso15 <juanjosemorenobenavides207@gmail.com>
2026-04-06 16:00:47 -0500

    M - error

--- src/main/java/com/gestion/micromarket/entity/Sales.java
+++ src/main/java/com/gestion/micromarket/entity/Sales.java
@@ -1,49 +1,4 @@
 package com.gestion.micromarket.entity;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lombok.Data;
-
-@Entity
-@Data
-@Table(name = "sales")
 public class Sales {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
-
-    @Column(name = "sale_date", nullable = false, updatable = false)
-    private LocalDateTime saleDate;
-
-    @Column(name = "subtotal", nullable = false, precision = 10, scale = 2)
-    private BigDecimal subtotal;
-
-    @Column(name = "vat", nullable = false, precision = 10, scale = 2)
-    private BigDecimal vat;
-
-    @Column(name = "total", nullable = false, precision = 10, scale = 2)
-    private BigDecimal total;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "employee_id", nullable = false)
-    private Employees employee;
-
-    @OneToMany(mappedBy = "sale", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<SaleDetail> saleDetails = new ArrayList<>();
 
 }
