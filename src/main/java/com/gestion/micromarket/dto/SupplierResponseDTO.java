cb4c99776ffd60e50362bedea3d40b4834df15f2
krisgallego <krisgallego7@gmail.com>
2026-04-06 11:52:17 -0500

    SupplierResponseDTO.java dto

--- src/main/java/com/gestion/micromarket/dto/SupplierResponseDTO.java
+++ src/main/java/com/gestion/micromarket/dto/SupplierResponseDTO.java
@@ -1,8 +1,18 @@
 package com.gestion.micromarket.dto;
 
+import java.time.LocalDateTime;
 
+import lombok.Data;
+
+@Data
 public class SupplierResponseDTO {
 
-    
+    private Long id;
+    private String name;
+    private String taxId;
+    private String phone;
+    private String address;
+    private String email;
+    private LocalDateTime createdAt;
 
 }
