99569b1141757741a5ed9203e5ee0727c39b55f4
krisgallego <krisgallego7@gmail.com>
2026-04-06 11:48:55 -0500

    SupplierRequestDTO.java dto

--- src/main/java/com/gestion/micromarket/dto/SupplierRequestDTO.java
+++ src/main/java/com/gestion/micromarket/dto/SupplierRequestDTO.java
@@ -1,8 +1,19 @@
 package com.gestion.micromarket.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class SupplierRequestDTO {
 
+    @NotBlank(message = "El nombre es obligatorio")
+    private String name;
 
-   
+    @NotBlank(message = "Se requiere identificacion fiscal")
+    private String taxId;
+
+    private String phone;
+    private String address;
+    private String email;
+
 }
