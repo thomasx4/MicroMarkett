915134422586f3708f62329bafbc65f00d2418af
rixhard-rxp <oskitarmolpa@gmail.com>
2026-05-10 21:14:37 -0500

    fix: Agregar Try Catch a los métodos de EmployeesController

--- src/main/java/com/gestion/micromarket/controller/EmployeesController.java
+++ src/main/java/com/gestion/micromarket/controller/EmployeesController.java
@@ -43,14 +43,21 @@ public class EmployeesController {
     @PostMapping()
     public ResponseEntity<MessageResponseDTO> createEmplyees(
             @Valid @RequestBody EmployeesRequestDTO employeesRequestDTO) {
-        MessageResponseDTO messageResponseDTO = employeesService.createrEmployees(employeesRequestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(messageResponseDTO);
+            try {
+                MessageResponseDTO messageResponseDTO = employeesService.createrEmployees(employeesRequestDTO);
+                return ResponseEntity.status(HttpStatus.CREATED).body(messageResponseDTO);
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new MessageResponseDTO(e.getMessage()));
+            }
+        
     }
 
     /**
-     * Obtiene la lista de todos los empleados 
+     * Obtiene la lista de todos los empleados
      * 
-     * @return List<EmployeesResponseDTO> Lista con todos los empleados (201) o si algo falla (400)
+     * @return List<EmployeesResponseDTO> Lista con todos los empleados (201) o si
+     *         algo falla (400)
      */
     @GetMapping()
     public ResponseEntity<List<EmployeesResponseDTO>> getAllEmployees() {
@@ -63,15 +70,19 @@ public class EmployeesController {
     }
 
     /**
-     * Busca un empleado por su id 
+     * Busca un empleado por su id
      * 
-     * @param id 
+     * @param id
      * @return Optional<EmployeesResponseDTO> Empleado encontrado (201)
      */
     @GetMapping("/{id}")
     public ResponseEntity<Optional<EmployeesResponseDTO>> getEmployeeById(@PathVariable Long id) {
-        Optional<EmployeesResponseDTO> response = employeesService.getEmployeeById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        try {
+            Optional<EmployeesResponseDTO> response = employeesService.getEmployeeById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
     /**
@@ -82,15 +93,21 @@ public class EmployeesController {
      */
     @GetMapping("/documentNumber/{documentNumber}")
     public ResponseEntity<EmployeesResponseDTO> getEmployeeByDocumentNumber(@PathVariable String documentNumber) {
-        EmployeesResponseDTO response = employeesService.getEmployeeByDocumentNumber(documentNumber);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        try {
+            EmployeesResponseDTO response = employeesService.getEmployeeByDocumentNumber(documentNumber);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        
     }
 
     /**
      * Obtiene los empleados que tienen un rol específico
      * 
      * @param role puede ser: administrator, cashier o assistant
-     * @return List<EmployeesResponseDTO> lista de empleados con el rol específico (201)
+     * @return List<EmployeesResponseDTO> lista de empleados con el rol específico
+     *         (201)
      * @throws RuntimeException si el valor de (rol) no es válido
      */
     @GetMapping("/role/{role}")
@@ -101,17 +118,21 @@ public class EmployeesController {
             throw new RuntimeException("El rol debe de ser 'administrator', 'cashier', o 'assistant' no: " + role);
         }
 
-        Role rolEnum = Role.valueOf(role);
-
-        List<EmployeesResponseDTO> response = employeesService.getEmployeesByRole(rolEnum);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        try {
+            Role rolEnum = Role.valueOf(role);
+            List<EmployeesResponseDTO> response = employeesService.getEmployeesByRole(rolEnum);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
     /**
      * Obtiene los empleados por su estado de actividad
      * 
      * @param active puede ser activo=true o inactivo=false
-     * @return List<EmployeesResponseDTO> lista de empleados con el estado específico (201)
+     * @return List<EmployeesResponseDTO> lista de empleados con el estado
+     *         específico (201)
      * @throws RuntimeException si el valor de (active) no es ni true ni false
      */
     @GetMapping("active/{active}")
@@ -121,25 +142,34 @@ public class EmployeesController {
             throw new RuntimeException("El valor debe ser 'true' o 'false', no: " + active);
         }
 
-        Boolean activeBool = Boolean.valueOf(active);
-        List<EmployeesResponseDTO> response = employeesService.getEmployyesByActive(activeBool);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        try {
+            Boolean activeBool = Boolean.valueOf(active);
+            List<EmployeesResponseDTO> response = employeesService.getEmployyesByActive(activeBool);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        } 
     }
 
     /**
      * Obtiene los empleados contratados dentro de un rango de fecha específico
      * 
-     * @param startDate 
+     * @param startDate
      * @param endDate
-     * @return List<EmployeesResponseDTO> lista de empleados contratados en el período específico (200)
+     * @return List<EmployeesResponseDTO> lista de empleados contratados en el
+     *         período específico (200)
      */
     @GetMapping("/hire-date-range")
     public ResponseEntity<List<EmployeesResponseDTO>> getEmployeesByHireDateRange(
             @RequestParam LocalDate startDate,
             @RequestParam LocalDate endDate) {
 
-        List<EmployeesResponseDTO> response = employeesService.getEmployeesByHireDateRange(startDate, endDate);
-        return ResponseEntity.ok(response);
+        try {
+            List<EmployeesResponseDTO> response = employeesService.getEmployeesByHireDateRange(startDate, endDate);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
     /**
@@ -152,12 +182,18 @@ public class EmployeesController {
     @PutMapping("/{id}")
     public ResponseEntity<EmployeesResponseDTO> updateEmployee(@PathVariable Long id,
             @RequestBody EmployeesRequestDTO employeesRequestDTO) {
-        EmployeesResponseDTO response = employeesService.updateEmployeeComplete(id, employeesRequestDTO);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        try {
+            EmployeesResponseDTO response = employeesService.updateEmployeeComplete(id, employeesRequestDTO);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        
     }
 
     /**
-     * Actualiza uno o más campos específicos de un empleado existente sin reemplazar el registro completo
+     * Actualiza uno o más campos específicos de un empleado existente sin
+     * reemplazar el registro completo
      * 
      * @param id
      * @param employeesRequestDTO
@@ -166,8 +202,12 @@ public class EmployeesController {
     @PatchMapping("/{id}")
     public ResponseEntity<EmployeesResponseDTO> updateSpecificEmployee(@PathVariable Long id,
             @RequestBody EmployeesRequestDTO employeesRequestDTO) {
-        EmployeesResponseDTO response = employeesService.updateEmployee(id, employeesRequestDTO);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        try {
+            EmployeesResponseDTO response = employeesService.updateEmployee(id, employeesRequestDTO);
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
     /**
@@ -179,9 +219,12 @@ public class EmployeesController {
     @DeleteMapping("/{id}")
 
     public ResponseEntity<MessageResponseDTO> deleteEmployeeByid(@PathVariable Long id) {
-        MessageResponseDTO messageResponseDTO = employeesService.deleteEmployeeByid(id);
-        return ResponseEntity.status(HttpStatus.OK).body(messageResponseDTO);
-
+        try {
+            MessageResponseDTO messageResponseDTO = employeesService.deleteEmployeeByid(id);
+            return ResponseEntity.status(HttpStatus.OK).body(messageResponseDTO);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
     /**
@@ -193,9 +236,11 @@ public class EmployeesController {
     @DeleteMapping("/documentNumber/{documentNumber}")
 
     public ResponseEntity<MessageResponseDTO> deleteEmployeeByDocumentNumber(@PathVariable String documentNumber) {
-        MessageResponseDTO messageResponseDTO = employeesService.deleteEmployeeByNumberDocument(documentNumber);
-        return ResponseEntity.status(HttpStatus.OK).body(messageResponseDTO);
-
+        try {
+            MessageResponseDTO messageResponseDTO = employeesService.deleteEmployeeByNumberDocument(documentNumber);
+            return ResponseEntity.status(HttpStatus.OK).body(messageResponseDTO);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
-
 }
